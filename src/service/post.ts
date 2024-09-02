91211b5cce18bda2fdbb8b87e26097e0667a9311
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-03 02:02:49 +0900

    FEAT: Add order-by-time at Post Service

--- src/service/post.ts
+++ src/service/post.ts
@@ -1,12 +1,12 @@
 import { Post } from "@/model/post"
 
-import { collection, query, getDocs } from "firebase/firestore"
+import { collection, query, getDocs, orderBy } from "firebase/firestore"
 import { firestore_DB } from "./firebase/firestore"
 
 const FIREBASE_POST_COLLECTION_NAME = "post"
 
-export const GetAllPosts = async () => {
-    const queried = query(collection(firestore_DB, FIREBASE_POST_COLLECTION_NAME))
+export const GetAllPostsOrderedByUploaded = async () => {
+    const queried = query(collection(firestore_DB, FIREBASE_POST_COLLECTION_NAME), orderBy("uploaded", "desc"))
     const snapshoted = await getDocs(queried)
 
     const posts = new Array<Post>()
