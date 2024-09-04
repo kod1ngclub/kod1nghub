1766dd4f7242ee335faf2720577a7d38255f0aee
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-05 00:35:38 +0900

    FEAT: Add flex component

---
+++ src/components/flex.tsx
@@ -0,0 +1,16 @@
+export enum FlexDirection { Row = "r" , Column = "c" }
+export enum FlexJustifying { Start = "st", SpaceBetween = "sb", Center = "c" }
+
+export interface FlexProps {
+    children: JSX.Element | Array<JSX.Element>,
+    direction: FlexDirection,
+    justifying: FlexJustifying
+}
+
+const Flex = ({ children, direction, justifying }: FlexProps) => (
+    <div className={`flex--${direction}-${justifying}`}>
+        { children }
+    </div>
+)
+
+export default Flex
