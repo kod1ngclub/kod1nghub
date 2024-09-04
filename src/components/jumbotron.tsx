431a315e83c76e8bedf6f85cce119988be7982cd
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-05 01:37:49 +0900

    FEAT: Add auto-vetical-centering to jumbotron

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -1,5 +1,5 @@
 export interface JumbotronProps { children: JSX.Element | Array<JSX.Element> }
 
-const Jumbotron = ({ children }: JumbotronProps) => <div className="jumbotron">{ children }</div>
+const Jumbotron = ({ children }: JumbotronProps) => <div className="jumbotron flex--c-c">{ children }</div>
 
 export default Jumbotron
