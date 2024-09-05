89a0685cf959cb908fa4dd91938826f2ae378344
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-05 12:50:00 +0900

    FEAT: Add 'major' property to service model

--- src/model/service.ts
+++ src/model/service.ts
@@ -7,4 +7,5 @@ export interface Service {
     name: string
     description: string
     link: Link
+    major: boolean
 }
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,6 +1,6 @@
 import { Service } from "@/model/service"
 
-import { collection, query, getDocs } from "firebase/firestore"
+import { collection, query, getDocs, where } from "firebase/firestore"
 import { firestore_DB } from "./firebase/firestore"
 
 const FIREBASE_SERVICE_COLLECTION_NAME = "service"
@@ -9,9 +9,31 @@ export const GetAllServices = async () => {
     const queried = query(collection(firestore_DB, FIREBASE_SERVICE_COLLECTION_NAME))
     const snapshoted = await getDocs(queried)
 
-    const contacts = new Array<Service>()
+    const services = new Array<Service>()
 
-    snapshoted.forEach(item => contacts.push(item.data() as Service))
+    snapshoted.forEach(item => services.push(item.data() as Service))
 
-    return contacts
+    return services
+}
+
+export const GetAllMajorServcies = async () => {
+    const queried = query(collection(firestore_DB, FIREBASE_SERVICE_COLLECTION_NAME), where("major", "==", true))
+    const snapshoted = await getDocs(queried)
+
+    const services = new Array<Service>()
+
+    snapshoted.forEach(item => services.push(item.data() as Service))
+
+    return services
+}
+
+export const GetAllMinorServcies = async () => {
+    const queried = query(collection(firestore_DB, FIREBASE_SERVICE_COLLECTION_NAME), where("major", "==", false))
+    const snapshoted = await getDocs(queried)
+
+    const services = new Array<Service>()
+
+    snapshoted.forEach(item => services.push(item.data() as Service))
+
+    return services
 }
