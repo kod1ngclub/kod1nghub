b61b18d33ee2aee981e2888382376ec29e8785d3
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-05 13:03:13 +0900

    FEAT: Implemented layout.tsx with nav component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
+import { Nav } from "@/components/nav"
+
+import "@/style/style.min.css"
+
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }> ) => {
     return (
         <html>
             <body>
+                <Nav />
                 {children}
             </body>
         </html>
