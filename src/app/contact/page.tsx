9a83cfcb4aba55bd3dc399b269b693cd641cbc9d
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-07 17:49:23 +0900

    FIX: Make DOMs about contact not depend on model & service without its link

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,21 +25,21 @@ const Contact = async () => {
                 <Link className="link" href={discord.link}>
                     <Flex direction={FlexDirection.Column} justifying={FlexJustifying.Center}>
                         <DiscordIcon color={null} size={64} />
-                        <span className="text">{discord.name}</span>
+                        <span className="text">Discord</span>
                     </Flex>
                 </Link>
                 
                 <Link className="link" href={email.link}>
                     <Flex direction={FlexDirection.Column} justifying={FlexJustifying.Center}>
                         <EmailIcon color={null} size={64} />
-                        <span className="text">{email.name}</span>
+                        <span className="text">Email</span>
                     </Flex>
                 </Link>
 
                 <Link className="link" href={phone.link}>
                     <Flex direction={FlexDirection.Column} justifying={FlexJustifying.Center}>
                         <PhoneIcon color={null} size={64} />
-                        <span className="text">{phone.name}</span>
+                        <span className="text">Phone Call</span>
                     </Flex>
                 </Link>
             </Grid>        
