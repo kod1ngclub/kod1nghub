8b52113c725fc62507946a8553128710c9da7f38
kod1ngclub <wiiwefwefbi@gmail.com>
2024-09-09 23:14:52 +0900

    FIX: Remove unnecessary text at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,9 @@ import { GetAllMajorServcies } from "@/service/service"
 import { GetContactByName  } from "@/service/contact"
 
 // components
-import Flex, { FlexDirection, FlexJustifying } from "@/components/flex"
 import Grid from "@/components/grid"
 import Card from "@/components/card"
 import Jumbotron from "@/components/jumbotron"
-
-import Link from "next/link"
 
 // utils
 import { NewID } from "@/utils/id"
@@ -25,11 +22,6 @@ const Home = async () => {
             <Jumbotron>
                 <h1 className="typo1">Kod1ngclub</h1>
                 <p className="text mb-2">코딩 동아리를 위한 코딩 동아리</p>
-
-                <Flex direction={FlexDirection.Row} justifying={FlexJustifying.Center}>
-                    <Link className="link" href={DISCORD.link}>Discord에 참가하기</Link>
-                </Flex>
-                <p className="text">아니면 아래에서 서비스 둘러보기</p>
             </Jumbotron>
 
             
